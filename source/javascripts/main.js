823dd8529d2484f9f489a2b38ee894dc6d2cd13f
Jahed Ahmed <jahed.public@gmail.com>
2015-06-30 17:45:22 +0100

    Added protips.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -6,11 +6,33 @@ $(function() {
         center: true,
         slideNumber: false,
         mouseWheel: true,
-
         transition: 'none',
 
         dependencies: []
     });
 
     hljs.initHighlightingOnLoad();
+
+    autoProtips();
+
+    Reveal.addEventListener( 'slidechanged', function( event ) {
+        autoProtips();
+        // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    } );
+
+    function autoProtips() {
+        var indices = Reveal.getIndices();
+        if(indices.h >= 4 && !Reveal.isAutoSliding()) {
+            Reveal.configure({
+                autoSlide: 10000,
+                transition: 'fade'
+            });
+
+            Reveal.toggleAutoSlide();
+        }
+
+        if(Reveal.getProgress() === 1) {
+            Reveal.slide(4);
+        }
+    }
 });
